197f5f32fc4c91b4da3986900684f2c6a214e564
blandflakes <xclite@gmail.com>
2016-01-20 23:34:58 -0500

    Updating to correspond with echo-chamber upgrade.

--- resources/leiningen/new/echo_chamber/app.clj
+++ resources/leiningen/new/echo_chamber/app.clj
@@ -3,16 +3,16 @@
             [echo.response :as response]))
 
 (defn hello-world
-  [session]
-  (response/respond session {:should-end? true
-                             :speech (response/plaintext-speech "Hello world!")
-                             :card (response/simple-card "Hello world" "Request received." "Goodbye!")}))
+  []
+  (response/respond {:should-end? true
+                     :speech (response/plaintext-speech "Hello world!")
+                     :card (response/simple-card "Hello world" "Request received. Goodbye!")}))
 
 ; FIXME: Implement your own logic below!
 (deftype App []
   echo/IEchoApp
-  (on-launch [this request session] (hello-world session))
-  (on-intent [this request session] (hello-world session))
-  (on-end [this request session] (hello-world session)))
+  (on-launch [this request session] (hello-world))
+  (on-intent [this request session] (hello-world))
+  (on-end [this request session] (hello-world)))
 
 (def app-handler (echo/request-dispatcher (App.)))
