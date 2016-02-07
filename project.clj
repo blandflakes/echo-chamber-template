9c8d2dbce643d551aefdb93275d675e49cef555d
blandflakes <xclite@gmail.com>
2016-02-07 09:08:23 -0500

    Bumping version of echo-chamber-middleware, which has a newer version of the signature verifier.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject echo-chamber/lein-template "0.1.6"
+(defproject echo-chamber/lein-template "0.1.7"
   :description "Generates a simple ring application for serving an Alexa skill"
   :url "https://github.com/blandflakes/echo-chamber-template"
   :license {:name "MIT License"
